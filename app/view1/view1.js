aace2a08192d1d31a9f8f6594819c9e197395492
Roman Gimadinov <aivazovskii@mail.ru>
2017-02-25 22:50:40 +0300

    Added angular leaflet directive + created first map

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'leaflet-directive'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+    $routeProvider.when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl'
+    });
 }])
 
 .controller('View1Ctrl', [function() {
